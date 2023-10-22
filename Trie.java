02006ebd8901fc19e73878551b9647f6a4538ba5
lucas-liang <lsliang732@gmail.com>
2023-10-22 12:08:13 -0700

    inputted some boilerplate

---
+++ Trie.java
@@ -0,0 +1,24 @@
+/**
+ * Implements a Trie data structure
+ * @author  Akbarli Sodikov
+ * @author  Lucas Almedia
+ * @author  Lucas Liang
+ * @author  Milton Rue  
+ */
+
+public class Trie{
+
+
+    public Trie(int size){
+
+    }
+
+    public Trie(){
+
+    }
+
+    
+
+
+
+}
---
+++ TrieTest.java
@@ -0,0 +1,14 @@
+/**
+ * Test code for the Trie data structure
+ * @author  Akbarli Sodikov
+ * @author  Lucas Almedia
+ * @author  Lucas Liang
+ * @author  Milton Rue  
+ */
+
+public class TrieTest {
+    public static void main(String[] args){
+        Trie t = new Trie();
+
+    }
+}
