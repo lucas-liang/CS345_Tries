59edb5a6f617d8aa3bff4452db46301dd2eab5e1
Lucas Almeida <lucasoa2701@live.com>
2023-10-30 19:14:11 -0700

    Added a few method implementations

--- Trie.java
+++ Trie.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * Implements a Trie data structure
  * @author  Akbarali Sodikov
@@ -14,51 +15,115 @@ public class Trie {
     }
 
     public void insert(String word) {
-        
-    }
-
-    public boolean search(String word) {
-       
-    }
-
-    public boolean startsWith(String prefix) {
-        
-    }
-
-    
-    // Finding words with a certain prefix 
-    public ArrayList<String> findWordsWithPrefix(String prefix) {
-        List<String> result = new ArrayList<>();
-        TrieNode node = root;
-        for (char c: prefix.toCharArray()) {
-            if (!node.children.containsKey(c)) {
-                return result; 
+        TrieNode curr = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            // init node for this char
+            Character currChar = word.charAt(i);
+            if (!curr.children.containsKey(word.charAt(i))) {
+                curr.children.put(currChar, new TrieNode(currChar));
             }
-            node = node.children.get(c);
+            curr = curr.children.get(currChar);
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public boolean doesWordExist(String word) {
+       
+        TrieNode curr = this.root;
+
+        for (int i = 0; i < word.length(); i++) {
+            // 
+            if (!curr.children.containsKey(word.charAt(i))) return false;
         }
 
-        findAllWordsFromNode(node, prefix, result);
+        return curr.isEndOfWord;
+    }
+
+    /**
+     * remove word from trie
+     * @param word word to remove
+     * @return  return true if exist, return false if not
+     */
+    public boolean removeWord(String word) {
+       TrieNode curr = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            // init node for this char
+            Character currChar = word.charAt(i);
+            if (!curr.children.containsKey(word.charAt(i))) return false;
+            // move pointer
+            curr = curr.children.get(currChar);
+        }
+
+        return curr.isEndOfWord;
+    }
+   
+
+    
+    /**
+     * get list of words that start with prefix
+     * @param prefix
+     * @return
+     */
+    public List<String> findWordsWithPrefix(String prefix) {
+        List<String> result = new ArrayList<>();
+        TrieNode curr = root;
+        for (Character c: prefix.toCharArray()) {
+            if (!curr.children.containsKey(c)) {
+                return result;
+            }
+            curr = curr.children.get(c);
+        }
+
+        findAllWords(prefix, curr, result, new ArrayList<Character>());
+
         return result;
     }
-    
-    // Counting words with a certain prefix
-    public int countWordsWithPrefix(String prefix) {
-        TrieNode node = root;
-        for (char c: prefix.toCharArray()) {
-            if (!node.children.containsKey(c)) {
-                return 0; 
-            }
-            node = node.children.get(c);
+
+    private void findAllWords(String prefix, TrieNode curr, List<String> currList, List<Character> path) {
+
+        if (curr == null) {
+            return;
         }
-        return countWordsFromNode(node);
+
+
+        if (curr.isEndOfWord) {
+            String res = prefix;
+            for (Character c : path) {
+                res += String.valueOf(c);
+            }
+            currList.add(res);
+        }
+
+        for (Character c : curr.children.keySet()) {
+            path.add(c);
+            findAllWords(prefix, curr.children.get(c), currList, path);
+            path.remove(path.size()-1);
+        }
     }
     
-    private TrieNode searchPrefix(String prefix) {
-        
+    /**
+     * counts the number of words that start with this prefix
+     * @param prefix  prefix in question
+     * @return count
+     */
+    public int countWordsWithPrefix(String prefix) {
+        return findWordsWithPrefix(prefix).size();
     }
 
     private class TrieNode {
+        Character key;
+        HashMap<Character,TrieNode> children;
+        boolean isEndOfWord;
+
         
+        TrieNode(Character key) {
+            this.key = key;
+            this.children = new HashMap<>();
+            isEndOfWord = false;
+        }
+        TrieNode(){
+            this(null);
+        }
     }
 }
 
