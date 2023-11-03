6efad8e14d824aa1df1c4a0eafecee4ae9a22709
lucas-liang <lsliang732@gmail.com>
2023-11-02 19:29:02 -0700

    refactored and added some tests

--- TrieTest.java
+++ TrieTest.java
@@ -9,11 +9,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class TrieTest {
     public static void main(String[] args) {
+        testBasic();
+        checkDuplicate();
+        testFile1();
+        testFile2();
+        
+    }
+    /**
+     * Tests basic insert and remove for small number of elements
+     */
+    private static void testBasic(){
         Trie t = new Trie();
-
         t.insert("hello");
         t.insert("hi");
         t.insert("bye");
@@ -33,8 +43,28 @@ public class TrieTest {
         
         System.out.println("All words in the Trie:");
         t.printAllWords();
+        System.out.println("------------------------------------------------------------ \n");
 
-        
+    }
+
+    /**
+     * Checks to ensure that duplicates are not being added to tree as separate entities.
+     */
+
+    private static void checkDuplicate(){
+        Trie t = new Trie();
+        t.insert("hello");
+        t.insert("hello");
+        System.out.println("The size of the trie is: " + t.size());
+        System.out.println("------------------------------------------------------------ \n");
+    } 
+
+    /**
+     * Tests insert using 1,000 unique word text file.
+     */
+
+    private static void testFile1(){
+        Trie t = new Trie();
         try {
             BufferedReader reader = new BufferedReader(new FileReader("uniqueWords.txt"));
             String line;
@@ -46,8 +76,50 @@ public class TrieTest {
             e.printStackTrace();
         }
 
-        
+        System.out.println("Total words in the file: 1000\n");
         System.out.println("Total words in the Trie after adding words from the file: " + t.size());
+
+        System.out.println("------------------------------------------------------------ \n");
     }
-}
+
+    /**
+     * Tests methods on 84,099 size text file.
+     */
+
+    private static void testFile2(){
+        Trie t = new Trie();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("engmix.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                t.insert(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total words in the file: 84099\n");
+        System.out.println("Total words in the Trie after adding words from the file: " + t.size());
+        List<String> words = t.findWordsWithPrefix("zy");
+        String wordsZY = "zygote, zygotes, zygotic, zymology, zymurgy";
+        System.out.println("Words that start with zy are: " + wordsZY + "\n");
+        System.out.println("The Trie found the following words start with zy: ");
+        for(String s : words){
+            System.out.println(s);
+        }
+        t.removeWord("zygote");
+        System.out.println("Was zygote removed: " + !(t.doesWordExist("zygote")) + "\n");
+        List<String> words2 = t.findWordsWithPrefix("zy");
+        System.out.println("Printing words that start with zy! \n");
+        for(String s : words2){
+            System.out.println(s);
+        }
+
+        
+
+        System.out.println("------------------------------------------------------------ \n");
+    }
+
+    }
 
